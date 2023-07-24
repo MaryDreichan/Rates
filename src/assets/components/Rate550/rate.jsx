18aff268466d8e7a6fc1eb94fd2a511be3914ea3
MaryDreichan <maria@texo.by>
2023-07-24 10:27:36 +0300

    add 550

--- src/assets/components/Rate550/rate.jsx
+++ src/assets/components/Rate550/rate.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import styles from './style.module.css';
 
 const Rate550 = () => {
+    const isUnlimited550 = true;
+
   return (
-    <div className={styles.rateContainer}>
+    <div className={`${styles.rateContainer} ${isUnlimited550 ? styles.highlighted : ''}`}>
       <div className={styles.unlimited}>Безлимитный 550</div>
       <div className={styles.price}>550 руб/мин</div>
       <div className={styles.speed}>до 100Мб/сек</div>
